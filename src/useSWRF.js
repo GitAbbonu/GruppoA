0e949b505b8252aa4355dd1285b8e69c4a079a97
Cipriano Mereu <ciprianomereu@gmail.com>
2023-04-19 12:16:03 +0200

    fix:option ,url

--- src/useSWRF.js
+++ src/useSWRF.js
@@ -3,61 +3,59 @@ import useSWR, { mutate } from "swr";
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 function useSWRF() {
+  const common_request = (data, method) => {
+    return {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data ? JSON.stringify(data) : null,
+      mode: "cors",
+    };
+  };
+
+  const PLANETS_URL = "http://localhost:5000/planets";
+
+  //CRUD----------------------------------------------------------------------
+
   //GetAll
-  const { data, error } = useSWR("http://localhost:5000/planets", fetcher);
+  const { data, error } = useSWR(PLANETS_URL, fetcher);
 
   //GetOne
   async function getOne(id) {
-    const res = await fetch("http://localhost:5000/planets/" + id, fetcher);
-    const json = res.json();
+    const res = await fetch(PLANETS_URL + "/" + id);
+    const json = await res.json();
     console.log(json);
     return json;
   }
 
   //Create
   async function create(data) {
-    const option = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-      mode: "cors",
-    };
+    const option = common_request(data, "POST");
 
-    const res = await fetch("http://localhost:5000/planets", option);
+    const res = await fetch(PLANETS_URL, option);
     const json = await res.json();
-    mutate("http://localhost:5000/planets");
+    mutate(PLANETS_URL);
     return json;
   }
 
   //Update
   async function update(id, data) {
-    const option = {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-      mode: "cors",
-    };
+    const option = common_request(data, "PUT");
 
-    const res = await fetch("http://localhost:5000/planets/" + id, option);
+    const res = await fetch(PLANETS_URL + "/" + id, option);
     const json = await res.json();
-    mutate("http://localhost:5000/planets");
+    mutate(PLANETS_URL);
     return json;
   }
 
   //Delete
   async function deletePlanet(id) {
-    const option = {
-      method: "DELETE",
-      mode: "cors",
-    };
+    const option = common_request(null, "DELETE");
 
-    await fetch("http://localhost:5000/planets/" + id, option);
+    await fetch(PLANETS_URL + "/" + id, option);
 
-    mutate("http://localhost:5000/planets");
+    mutate(PLANETS_URL);
   }
 
   return { data, error, getOne, create, update, deletePlanet };
